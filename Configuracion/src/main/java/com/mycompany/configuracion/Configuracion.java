a5412d66a5642bec81a71c098b9dc44f0556b1d9
luis comezaquira <lecomezaquira83@ucatolica.edu.co>
2024-08-22 17:24:19 -0500

    primer trabajo

---
+++ Configuracion/src/main/java/com/mycompany/configuracion/Configuracion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.configuracion;
+
+/**
+ *
+ * @author User
+ */
+public class Configuracion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
